6ed328a3aa9d7f841e5149d6f83ad53853fa84f0
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2023-05-30 22:38:12 +0100

    5.ReviewCard completed

--- utils.js
+++ utils.js
@@ -1,15 +1,28 @@
-import axios from "axios"
+import axios from "axios";
 
 const GamesApi = axios.create({
-    baseURL: "https://nc-games-1ybo.onrender.com/api"
-})
+  baseURL: "https://nc-games-1ybo.onrender.com/api",
+});
 
 export const getReviews = () => {
-    let path = '/reviews'
+//   let path = "/reviews";
+  return GamesApi.get("/reviews").then(({ data }) => {
+    return data.review;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+};
+
+export const getReviewId = (review_id) => {
+    // console.log(review_id)
     return GamesApi
-    .get(path)
-    .then(({data})=>{
-        // console.log(data.review)
-        return data.review
+    .get(`/reviews/${review_id}`)
+    .then(( review ) => {
+        // console.log(review.data)
+      return review.data;
     })
-}
+    .catch((err) => {
+        // console.log(err);
+      });
+  };
