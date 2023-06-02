e5cb1236291519d239f139faa1b9237244ab79ec
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2023-06-02 09:11:20 +0100

    7.Vote

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,34 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { patchVotes } from "../../utils";
 
 function Votes({ review_id, votes, review }) {
   const [voteChange, setVoteChange] = useState(0);
   const [errorMessage, setErrorMessage] = useState("");
 
-  
   const handleVotes = (idNum) => {
-    // console.log("handleVotes called");
     setVoteChange((currVote) => currVote + 1);
     patchVotes(idNum)
-      .then(() => {
-        // console.log("patchVotes success");
-      })
-      .catch((err) => {
-        console.log("patchVotes error", err);
-        setVoteChange((currVote) => currVote - 1);
-        setErrorMessage("Issue with voting, please try again later");
-      });
+    .catch((err) => {
+      setVoteChange((currVote) => currVote - 1);
+      setErrorMessage("Issue with voting, please try again later");
+    });
   };
-  
-//   console.log("Votes component rendered");
-  
+
   return (
     <button
-      onClick={() => {handleVotes(review_id)}}
+      onClick={() => {
+        handleVotes(review_id);
+      }}
+      disabled={voteChange !== 0}
     >
-      👍🏼 {review.votes + voteChange}
+      <span aria-label="Up vote this review">
+        👍🏽 {review.votes + voteChange}
+      </span>
     </button>
-    
   );
 }
 export default Votes;
