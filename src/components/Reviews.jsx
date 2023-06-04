9bbf288cc2c679e284750db8fbf157a1ada29f30
Nadia Ibrahim <121519025+NadiaIb@users.noreply.github.com>
2023-06-04 19:03:31 +0100

    created url links

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { getReviews } from "../../api";
+import { useSearchParams } from "react-router-dom";
 
 function Reviews() {
   const [listReviews, setListReviews] = useState([]);
   const [isLoadingReviews, setIsLoadingReviews] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const sortByQuery = searchParams.get("sort_by"); // "title"
+    const orderQuery = searchParams.get('order'); // "title"
 
   useEffect(() => {
     getReviews()
@@ -12,13 +17,49 @@ function Reviews() {
         setIsLoadingReviews(false);
       })
       .catch((err) => {});
-  }, []);
+  }, [sortByQuery,orderQuery]);
+
+  const handleSortChange = (event) => {
+    const sortByValue = event.target.value;
+
+    // copy existing queries to avoid mutation
+    const newParams = new URLSearchParams(searchParams);
+  
+    // set the sort_by query
+    if (sortByValue) {
+      newParams.set("sort_by", sortByValue);
+    } else {
+      newParams.delete("sort_by");
+    }
+  
+    // update the search params
+    setSearchParams(newParams);
+  };
+  
+  const setSortOrder = (direction) => {
+    // copy existing queries to avoid mutation
+    const newParams = new URLSearchParams(searchParams);
+    // set the order query
+    newParams.set('order', direction);
+    setSearchParams(newParams);
+  };
+
 
   if (isLoadingReviews) {
     return <h2> Loading Reviews...</h2>;
   } else {
     return (
       <section>
+        <select onChange={handleSortChange}>
+          <option value="">Sort By</option>
+          <option value="created_at">Date</option>
+          <option value="votes">Votes</option>
+          <option value="title">Title</option>
+        </select>
+
+        <button onClick={() => setSortOrder("asc")}>⬆</button>
+        <button onClick={() => setSortOrder("desc")}>⬇</button>
+
         <h2> All Reviews ({listReviews.length}) </h2>
         <ul>
           {listReviews.map((review) => {
@@ -37,8 +78,7 @@ function Reviews() {
                 </p>
                 <p>Votes: {review.votes}</p>
                 <a href={`/reviews/${review.review_id}`}> Read full review </a>
-              </li>           
-              
+              </li>
             );
           })}
         </ul>
